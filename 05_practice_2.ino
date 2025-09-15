ff45d80ab4614d7c2dcb4ea63776db3ae2e51f36
bomin676 <bomin060706@naver.com>
2025-09-15 11:10:14 +0900

    Update 05_practice_2.ino

--- 05_practice_2.ino
+++ 05_practice_2.ino
@@ -9,9 +9,9 @@ void loop() {
   delay(1000);
   for (int i=0;i<5;i++) {
     digitalWrite(PIN_LED,1);
-    delay(200);
+    delay(100);
     digitalWrite(PIN_LED,0);
-    delay(200);
+    delay(100);
   }
   digitalWrite(PIN_LED,1);
   while(1){
