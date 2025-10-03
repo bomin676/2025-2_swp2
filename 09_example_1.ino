bfcdf497d4a93515e2fc6609a93f7d5716f895f7
bomin676 <bomin060706@naver.com>
2025-10-03 16:30:52 +0900

    09C19 update

--- 09_example_1.ino
+++ 09_example_1.ino
@@ -1,96 +1,179 @@
-// Arduino pin assignment
-#define PIN_LED  9
+#define PIN_LED 9
 #define PIN_TRIG 12
 #define PIN_ECHO 13
 
-// configurable parameters
-#define SND_VEL 346.0     // sound velocity at 24 celsius degree (unit: m/sec)
-#define INTERVAL 25       // sampling interval (unit: msec)
-#define PULSE_DURATION 10 // ultra-sound Pulse Duration (unit: usec)
-#define _DIST_MIN 100     // minimum distance to be measured (unit: mm)
-#define _DIST_MAX 300     // maximum distance to be measured (unit: mm)
+#define SND_VEL 346.0
+#define INTERVAL 25
+#define PULSE_DURATION 10
+#define _DIST_MIN 100
+#define _DIST_MAX 300
 
-#define TIMEOUT ((INTERVAL / 2) * 1000.0) // maximum echo waiting time (unit: usec)
-#define SCALE (0.001 * 0.5 * SND_VEL)     // coefficent to convert duration to distance
+#define TIMEOUT ((INTERVAL / 2) * 1000.0)
+#define SCALE (0.001 * 0.5 * SND_VEL)
 
-#define _EMA_ALPHA 0.5    // EMA weight of new sample (range: 0 to 1)
-                          // Setting EMA to 1 effectively disables EMA filter.
+#define _EMA_ALPHA 0.5
+#define N_SAMPLES 30
 
-// global variables
-unsigned long last_sampling_time;   // unit: msec
-float dist_prev = _DIST_MAX;        // Distance last-measured
-float dist_ema;                     // EMA distance
+unsigned long last_sampling_time;
+float dist_prev = _DIST_MAX;
+float dist_ema;
+
+float samples[N_SAMPLES];
+int sample_index = 0;
 
 void setup() {
-  // initialize GPIO pins
-  pinMode(PIN_LED,OUTPUT);
-  pinMode(PIN_TRIG,OUTPUT);
-  pinMode(PIN_ECHO,INPUT);
+  pinMode(PIN_LED, OUTPUT);
+  pinMode(PIN_TRIG, OUTPUT);
+  pinMode(PIN_ECHO, INPUT);
   digitalWrite(PIN_TRIG, LOW);
 
-  // initialize serial port
   Serial.begin(57600);
 }
 
 void loop() {
-  float dist_raw, dist_filtered;
-  
-  // wait until next sampling time. 
-  // millis() returns the number of milliseconds since the program started. 
-  // will overflow after 50 days.
+  float dist_raw, dist_median;
+
   if (millis() < last_sampling_time + INTERVAL)
     return;
 
-  // get a distance reading from the USS
-  dist_raw = USS_measure(PIN_TRIG,PIN_ECHO);
+  dist_raw = USS_measure(PIN_TRIG, PIN_ECHO);
 
-  // Modify the below if-else statement to implement the range filter
-  if ((dist_raw == 0.0) || (dist_raw > _DIST_MAX)) {
-      dist_filtered = dist_raw;
-  } else if (dist_raw < _DIST_MIN) {
-      dist_filtered = dist_raw;
-  } else {    // In desired Range
-      dist_filtered = dist_raw;
-      dist_prev = dist_raw;
+  samples[sample_index] = dist_raw;
+  sample_index = (sample_index + 1) % N_SAMPLES;
+
+  float temp_samples[N_SAMPLES];
+  for (int i = 0; i < N_SAMPLES; i++) {
+    temp_samples[i] = samples[i];
   }
+  sort_array(temp_samples, N_SAMPLES);
 
-  // Modify the below line to implement the EMA equation
-  dist_ema = dist_filtered;
+  dist_median = temp_samples[N_SAMPLES / 2];
 
-  // output the distance to the serial port
-  Serial.print("Min:");   Serial.print(_DIST_MIN);
-  Serial.print(",raw:"); Serial.print(min(dist_raw, _DIST_MAX + 100));
-  Serial.print(",filtered:");  Serial.print(min(dist_filtered, _DIST_MAX + 100));
-  Serial.print(",ema:");  Serial.print(min(dist_ema, _DIST_MAX + 100));
-  Serial.print(",Max:");  Serial.print(_DIST_MAX);
+  Serial.print("Min:");
+  Serial.print(_DIST_MIN);
+  Serial.print(",raw:");
+  Serial.print(dist_raw);
+  Serial.print(",ema:");
+  Serial.print(dist_ema);
+  Serial.print(",median:");
+  Serial.print(dist_median);
+  Serial.print(",Max:");
+  Serial.print(_DIST_MAX);
   Serial.println("");
 
-  // do something here
-  if ((dist_ema < _DIST_MIN) || (dist_ema > _DIST_MAX))
-    digitalWrite(PIN_LED, 1);       // LED OFF
+  if ((dist_raw < _DIST_MIN) || (dist_median > _DIST_MAX))
+    digitalWrite(PIN_LED, 1);
   else
-    digitalWrite(PIN_LED, 0);       // LED ON
+    digitalWrite(PIN_LED, 0);
 
-  // update last sampling time
   last_sampling_time += INTERVAL;
 }
 
-// get a distance reading from USS. return value is in millimeter.
-float USS_measure(int TRIG, int ECHO)
-{
+float USS_measure(int TRIG, int ECHO) {
   digitalWrite(TRIG, HIGH);
   delayMicroseconds(PULSE_DURATION);
   digitalWrite(TRIG, LOW);
-  
-  return pulseIn(ECHO, HIGH, TIMEOUT) * SCALE; // unit: mm
 
-  // Pulse duration to distance conversion example (target distance = 17.3m)
-  // - pulseIn(ECHO, HIGH, timeout) returns microseconds (음파의 왕복 시간)
-  // - 편도 거리 = (pulseIn() / 1,000,000) * SND_VEL / 2 (미터 단위)
-  //   mm 단위로 하려면 * 1,000이 필요 ==>  SCALE = 0.001 * 0.5 * SND_VEL
-  //
-  // - 예, pusseIn()이 100,000 이면 (= 0.1초, 왕복 거리 34.6m)
-  //        = 100,000 micro*sec * 0.001 milli/micro * 0.5 * 346 meter/sec
-  //        = 100,000 * 0.001 * 0.5 * 346
-  //        = 17,300 mm  ==> 17.3m
+  return pulseIn(ECHO, HIGH, TIMEOUT) * SCALE;
+}
+
+void sort_array(float arr[], int n) {
+  for (int i = 0; i < n - 1; i++) {
+    for (int j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        float temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+}#define PIN_LED 9
+#define PIN_TRIG 12
+#define PIN_ECHO 13
+
+#define SND_VEL 346.0
+#define INTERVAL 25
+#define PULSE_DURATION 10
+#define _DIST_MIN 100
+#define _DIST_MAX 300
+
+#define TIMEOUT ((INTERVAL / 2) * 1000.0)
+#define SCALE (0.001 * 0.5 * SND_VEL)
+
+#define _EMA_ALPHA 0.5
+#define N_SAMPLES 30
+
+unsigned long last_sampling_time;
+float dist_prev = _DIST_MAX;
+float dist_ema;
+
+float samples[N_SAMPLES];
+int sample_index = 0;
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+  pinMode(PIN_TRIG, OUTPUT);
+  pinMode(PIN_ECHO, INPUT);
+  digitalWrite(PIN_TRIG, LOW);
+
+  Serial.begin(57600);
+}
+
+void loop() {
+  float dist_raw, dist_median;
+
+  if (millis() < last_sampling_time + INTERVAL)
+    return;
+
+  dist_raw = USS_measure(PIN_TRIG, PIN_ECHO);
+
+  samples[sample_index] = dist_raw;
+  sample_index = (sample_index + 1) % N_SAMPLES;
+
+  float temp_samples[N_SAMPLES];
+  for (int i = 0; i < N_SAMPLES; i++) {
+    temp_samples[i] = samples[i];
+  }
+  sort_array(temp_samples, N_SAMPLES);
+
+  dist_median = temp_samples[N_SAMPLES / 2];
+
+  Serial.print("Min:");
+  Serial.print(_DIST_MIN);
+  Serial.print(",raw:");
+  Serial.print(dist_raw);
+  Serial.print(",ema:");
+  Serial.print(dist_ema);
+  Serial.print(",median:");
+  Serial.print(dist_median);
+  Serial.print(",Max:");
+  Serial.print(_DIST_MAX);
+  Serial.println("");
+
+  if ((dist_raw < _DIST_MIN) || (dist_median > _DIST_MAX))
+    digitalWrite(PIN_LED, 1);
+  else
+    digitalWrite(PIN_LED, 0);
+
+  last_sampling_time += INTERVAL;
+}
+
+float USS_measure(int TRIG, int ECHO) {
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(PULSE_DURATION);
+  digitalWrite(TRIG, LOW);
+
+  return pulseIn(ECHO, HIGH, TIMEOUT) * SCALE;
+}
+
+void sort_array(float arr[], int n) {
+  for (int i = 0; i < n - 1; i++) {
+    for (int j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        float temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
 }
